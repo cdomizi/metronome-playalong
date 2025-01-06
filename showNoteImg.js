e9a8a60c4a909cc0861868410f9c851a231df634
cdomizi <domizicesare@gmail.com>
2025-01-06 19:43:17 +0100

    Updates

--- showNoteImg.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
-const noteImgElement = document.querySelector("img#note");
-export function showNoteImg(url) {
-    // Display the SVG image
-    noteImgElement.setAttribute("src", url);
-}
